768f1fd7b907caedb686e2c6a24d2546bbf81144
Milan Perera <milanperera1993@gmail.com>
2019-07-02 17:09:54 +0530

    mongo db setup, swagger config, initial todo service set up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    TodosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
